5220bf2c522612f90a4dac8301b2a3cb0cc1fbb1
Nazim Boudeffa <boudeffa@gmail.com>
2024-04-04 05:19:22 +0200

    text center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,24 +20,24 @@ export default function Home() {
       </Link>
     </section>
     <section className="flex flex-col items-center justify-center min-h-screen py-2">
-      <p>
+      <p className="text-xl mx-3 text-center">
         I do not know what to put here, but I am sure it will be great.
       </p>
-      <p>
+      <p className="text-xl mx-3 text-center">
         I do not know how to evaluate time and budget, but I am sure that the problem will be solved.
       </p>
-      <p>
+      <p className="text-xl mx-3 text-center">
         Just take it easy mankind.
       </p>
     </section>
     <section className="flex flex-col items-center justify-center min-h-screen py-2 bg-green-700">
-      <p className="text-white">
+      <p className="text-white text-xl mx-3 text-center">
         You can subscribe to my newsletter to get the latest news.
       </p>
-      <p className="text-white">
+      <p className="text-white text-xl mx-3 text-center">
         I write in french and english.
       </p>
-      <p className="text-white">
+      <p className="text-white text-xl mx-3 text-center">
         <Link
           href="https://nazimboudeffa.substack.com"
         > 
